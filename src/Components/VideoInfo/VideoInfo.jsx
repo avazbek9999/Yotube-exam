6058a0f0c79fe07a08a2c5fa6525fde917f1c21d
avazbek9999 <avazbe97salimov@yandex.ru>
2021-11-28 21:59:51 +0500

    initial commit

--- src/Components/VideoInfo/VideoInfo.jsx
+++ src/Components/VideoInfo/VideoInfo.jsx
@@ -9,42 +9,44 @@ function VideoInfo() {
 	return (
 		<>
 			<div className='video__description'>
-				<NavLink to='/channel'>
-					<img
-						className='video__description__avatar'
-						src={Food}
-						alt='Food'
-						width={80}
-						height={80}
-					/>
-				</NavLink>
-				<div className='about-video'>
-					<div className='video__channel-name'>
-						<h3 className='video__channel__headding'>
-							Food & Drink
-						</h3>
-						<p className='published-date'>
-							Published on 14 Jun 2019
-						</p>
-					</div>
+				<div className='video__description__inner'>
+					<NavLink to='/channel'>
+						<img
+							className='video__description__avatar'
+							src={Food}
+							alt='Food'
+							width={80}
+							height={80}
+						/>
+					</NavLink>
+					<div className='about-video'>
+						<div className='video__channel-name'>
+							<h3 className='video__channel__headding'>
+								Food & Drink
+							</h3>
+							<p className='published-date'>
+								Published on 14 Jun 2019
+							</p>
+						</div>
 
-					<div className='about-video__text'>
-						<p className='about-video-paragrf'>
-							A successful marketing plan relies heavily on the
-							pulling-power of advertising copy. Writing
-							result-oriented ad copy is difficult, as it must
-							appeal to, entice, and convince consumers to take
-							action. There is no magic formula to write perfect
-							ad copy; it is based on a number of factors,
-							including ad placement, demographic, even the
-							consumer’s mood when they see your ad.{' '}
-						</p>
+						<div className='about-video__text'>
+							<p className='about-video-paragrf'>
+								A successful marketing plan relies heavily on
+								the pulling-power of advertising copy. Writing
+								result-oriented ad copy is difficult, as it must
+								appeal to, entice, and convince consumers to
+								take action. There is no magic formula to write
+								perfect ad copy; it is based on a number of
+								factors, including ad placement, demographic,
+								even the consumer’s mood when they see your ad.{' '}
+							</p>
 
-						<span className='about__show'>Show more</span>
+							<span className='about__show'>Show more</span>
+						</div>
 					</div>
 				</div>
 
-				<div>
+				<div className='vidio__subscribbtn'>
 					<SubscripBtn />
 				</div>
 			</div>
